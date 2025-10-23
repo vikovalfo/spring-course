43a62c4241c5df5312f6dcd2830718dec17fa20d
Alessa Valencia <vector386@gmail.com>
2025-10-22 20:04:21 -0600

    model attribute template population through thymeleaf and controller

--- src/main/java/com/andres/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/andres/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.andres.curso.springboot.webapp.springbootweb.models.User;
 
@@ -18,22 +19,24 @@ public class UserController {
 
         User user = new User("Alessa", "Valencia");
         user.setEmail("alessa@email.com");
-        model.addAttribute("title", "Hello world Spring Boot");
+        model.addAttribute("title", "Hola World Spring Boot");
         model.addAttribute("user", user);
         return "details";
     }
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = Arrays.asList(
+        model.addAttribute("title", "List of users!");
+        return "list";
+    }
+    
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        return Arrays.asList(
                 new User("Pepa", "Gonzalez"),
                 new User("Lalo", "Perez", "lalo@email.com"),
                 new User("Juanita", "Roe", "juana@email.com"),
                 new User("Andres", "Doe"));
-
-        model.addAttribute("users", users);
-        model.addAttribute("title", "List of users!");
-        return "list";
     }
 
 }
