9013ac15d4d83890e6432d5203e7ee49382362ee
Alessa Valencia <vector386@gmail.com>
2025-11-21 19:55:46 -0600

    Refactor for the location of dto's

---
+++ src/main/java/com/vikovalfo/springboot_course/spring_mvc/models/dto/ParamDto.java
@@ -0,0 +1,22 @@
+package com.vikovalfo.springboot_course.spring_mvc.models.dto;
+
+public class ParamDto {
+    private String message;
+    private Integer code;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/vikovalfo/springboot_course/spring_mvc/models/dto/UserDto.java
@@ -0,0 +1,23 @@
+package com.vikovalfo.springboot_course.spring_mvc.models.dto;
+
+import com.vikovalfo.springboot_course.spring_mvc.models.User;
+
+public class UserDto {
+    
+    private String title;
+    private User user;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+}
