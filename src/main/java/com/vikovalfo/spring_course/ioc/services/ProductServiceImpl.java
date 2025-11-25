58fef02b4b9aed8d06ed37579ea7eac820f772b2
Alessa Valencia <vector386@gmail.com>
2025-11-25 14:38:27 -0600

    Dependency injection using constructor's approach

--- src/main/java/com/vikovalfo/spring_course/ioc/services/ProductServiceImpl.java
+++ src/main/java/com/vikovalfo/spring_course/ioc/services/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package com.vikovalfo.spring_course.ioc.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vikovalfo.spring_course.ioc.models.Product;
@@ -14,8 +13,8 @@ public class ProductServiceImpl implements ProductService {
 
 	private ProductRepository productRepository;
 
-	@Autowired
-	public void setProductRepository(ProductRepository productRepository) {
+	public ProductServiceImpl(ProductRepository productRepository) {
+		super();
 		this.productRepository = productRepository;
 	}
 
