5e625d17d47551e18e32f8d3da20ab6892638866
Alessa Valencia <vector386@gmail.com>
2025-11-25 17:30:36 -0600

    User defined bean definition using method specification instead of class's one

--- src/main/java/com/vikovalfo/spring_course/ioc/config/AppConfig.java
+++ src/main/java/com/vikovalfo/spring_course/ioc/config/AppConfig.java
@@ -1,9 +1,20 @@
 package com.vikovalfo.spring_course.ioc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+
+import com.vikovalfo.spring_course.ioc.repositories.ProductRepository;
+import com.vikovalfo.spring_course.ioc.repositories.ProductRepositoryJson;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
+
+	@Bean("productJson")
+	@Primary
+	ProductRepository productRepositoryJson() {
+		return new ProductRepositoryJson();
+	}
 }
