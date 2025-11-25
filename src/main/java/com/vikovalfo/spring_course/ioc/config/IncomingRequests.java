67fec676666465edac6f733f63471691b39adc26
Alessa Valencia <vector386@gmail.com>
2025-11-25 14:24:54 -0600

    Added some configuration for the logging of the incoming requests

---
+++ src/main/java/com/vikovalfo/spring_course/ioc/config/IncomingRequests.java
@@ -0,0 +1,20 @@
+package com.vikovalfo.spring_course.ioc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class IncomingRequests {
+
+	@Bean
+	CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
+	}
+
+}
