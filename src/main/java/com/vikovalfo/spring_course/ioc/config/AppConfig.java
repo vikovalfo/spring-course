020829e561aee065399d11237f4fbb9c6a430613
Alessa Valencia <vector386@gmail.com>
2025-11-25 18:44:26 -0600

    Updated dependency injection by method using resources declared and passed through arguments using the constructor

--- src/main/java/com/vikovalfo/spring_course/ioc/config/AppConfig.java
+++ src/main/java/com/vikovalfo/spring_course/ioc/config/AppConfig.java
@@ -1,9 +1,11 @@
 package com.vikovalfo.spring_course.ioc.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.vikovalfo.spring_course.ioc.repositories.ProductRepository;
 import com.vikovalfo.spring_course.ioc.repositories.impl.ProductRepositoryJson;
@@ -12,9 +14,12 @@ import com.vikovalfo.spring_course.ioc.repositories.impl.ProductRepositoryJson;
 @PropertySource("classpath:config.properties")
 public class AppConfig {
 
+	@Value("classpath:json/product.json")
+	private Resource resource;
+
 	@Bean("productJson")
 	@Primary
 	ProductRepository productRepositoryJson() {
-		return new ProductRepositoryJson();
+		return new ProductRepositoryJson(resource);
 	}
 }
--- src/main/java/com/vikovalfo/spring_course/ioc/repositories/impl/ProductRepositoryJson.java
+++ src/main/java/com/vikovalfo/spring_course/ioc/repositories/impl/ProductRepositoryJson.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.vikovalfo.spring_course.ioc.models.Product;
 import com.vikovalfo.spring_course.ioc.repositories.ProductRepository;
@@ -16,11 +17,19 @@ public class ProductRepositoryJson implements ProductRepository {
 
 	private List<Product> list;
 
+	public ProductRepositoryJson(Resource resource) {
+		readvalueWithResource(resource);
+	}
+
 	public ProductRepositoryJson() {
 		ClassPathResource resource = new ClassPathResource("/json/product.json", getClass());
+		readvalueWithResource(resource);
+	}
+
+	private void readvalueWithResource(Resource resource) {
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
-			list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+			list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class));
 		} catch (JacksonException | IOException e) {
 			e.printStackTrace();
 		}
