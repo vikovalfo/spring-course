146c58caecda3d4f170016de81fe58dea0139e14
Alessa Valencia <vector386@gmail.com>
2025-12-03 22:19:28 -0600

    Initial commit for interceptors

--- src/main/java/com/vikovalfo/spring_course/interceptors/SpringCourseApplication.java
+++ src/main/java/com/vikovalfo/spring_course/interceptors/SpringCourseApplication.java
@@ -1,4 +1,4 @@
-package com.vikovalfo.spring_course;
+package com.vikovalfo.spring_course.interceptors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
