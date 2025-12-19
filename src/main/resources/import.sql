80d062ed091f02a6881ad11784e1e8734135e0af
Alessa Valencia <vector386@gmail.com>
2025-12-19 15:55:13 -0600

    Added auto-import for data population

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO people (name, last_name, programming_language) VALUES ('Andres', 'Guzman', 'Java');
+INSERT INTO people (name, last_name, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO people (name, last_name, programming_language) VALUES ('John', 'Dow', 'JavaScript');
+INSERT INTO people (name, last_name, programming_language) VALUES ('Maria', 'Roe', 'Java');
+INSERT INTO people (name, last_name, programming_language) VALUES ('Josefa', 'Rae', 'Java');
+INSERT INTO people (name, last_name, programming_language) VALUES ('Alessa', 'Valencia', 'C++');
