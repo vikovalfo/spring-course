09f31198309ad9cad4ac99bc4573cbd67e4d57fb
Alessa Valencia <vector386@gmail.com>
2025-12-19 17:40:43 -0600

    Updated the person repository with overloaded query methods

--- src/main/java/com/vikovalfo/spring_course/jpa_hibernate/repositories/PersonRepository.java
+++ src/main/java/com/vikovalfo/spring_course/jpa_hibernate/repositories/PersonRepository.java
@@ -14,4 +14,13 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
     @Query("SELECT p FROM Person p WHERE p.programmingLanguage = ?1")
     List<Person> searchingByProgrammingLanguage(String programmingLanguage);
 
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p")
+    List<Object[]> gettingPersonData();
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.programmingLanguage = ?1")
+    List<Object[]> gettingPersonData(String programmingLanguage);
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.name = ?1 AND p.programmingLanguage = ?2")
+    List<Object[]> gettingPersonData(String name, String programmingLanguage);
+
 }
